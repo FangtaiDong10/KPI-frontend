b580550ff5932a52f5c1025333f365af7b1fcd5d
FangtaiDong10 <fangtaidong10@gmail.com>
2022-07-17 16:08:53 +1000

    initialization of frontend

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+import { NConfigProvider, NGlobalStyle } from 'naive-ui';
+</script>
+
+<template>
+  <n-config-provider>
+    <n-global-style />
+    <RouterView />
+  </n-config-provider>
+</template>
+
+<style>
+@import '@/assets/base.css';
+</style>
