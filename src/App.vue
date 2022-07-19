6b8744bda9a0e8d421ab65628a289d3d95bb2831
FangtaiDong10 <fangtaidong10@gmail.com>
2022-07-19 11:09:44 +1000

    login component update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router';
-import { NConfigProvider, NGlobalStyle, darkTheme } from 'naive-ui';
+import {
+  NConfigProvider,
+  NGlobalStyle,
+  darkTheme,
+  NMessageProvider,
+} from 'naive-ui';
 </script>
 
 <template>
   <n-config-provider :theme="darkTheme">
-    <n-global-style />
-    <RouterView />
+    <n-message-provider>
+      <RouterView />
+      <n-global-style />
+    </n-message-provider>
   </n-config-provider>
 </template>
 <style>
